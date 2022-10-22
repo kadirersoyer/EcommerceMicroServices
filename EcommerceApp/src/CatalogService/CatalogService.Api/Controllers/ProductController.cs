9b7d2ac689fb2eab329d9688a81b29a25b3e1daa
kadirersoyer <kadirersoyer@gmail.com>
2022-10-22 19:48:41 +0300

    N-Layers added

---
+++ EcommerceApp/src/CatalogService/CatalogService.Api/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductController()
+        {
+
+        }
+        //[HttpGet("{catalog}",Name = "GetAllProducts")]
+        [HttpGet("[action]/{catalog}", Name = "GetAllProducts")]
+        public async Task<IActionResult> GetProducts(string catalog)
+        {
+            return Ok();
+        }
+
+    }
+}
