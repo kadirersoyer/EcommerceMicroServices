9a74fdf5d64c163d51b4fc94bc498b4b2d994162
kadirersoyer <kadirersoyer@gmail.com>
2022-10-28 17:13:04 +0300

    #Catalog Service# Configuration Settings Added

--- EcommerceApp/src/CatalogService/CatalogService.Api/Program.cs
+++ EcommerceApp/src/CatalogService/CatalogService.Api/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-ServiceInjection.Inject(builder.Services);
+ServiceInjection.Inject(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 SeedContext.SeedData(app);
--- EcommerceApp/src/CatalogService/CatalogService.Shared/ServiceInjection.cs
+++ EcommerceApp/src/CatalogService/CatalogService.Shared/ServiceInjection.cs
@@ -6,15 +6,23 @@ using CatalogService.Infrastructure.UoW;
 using CatalogService.Shared.Commands;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CatalogService.Shared
 {
     public static class ServiceInjection
     {
-        public static void Inject(IServiceCollection services)
+        public static void Inject(IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<CatalogContext>(options => options.UseSqlServer("Data Source=sql_server_container; User Id=sa; Initial Catalog=CatalogDB; Password=Password4598."));
+
+            var dataSource = configuration.GetConnectionString("Server");
+            var database = configuration.GetConnectionString("Database");
+            var userId = configuration.GetConnectionString("User");
+            var password = configuration.GetConnectionString("Password");
+
+            services.AddDbContext<CatalogContext>(options => options.UseSqlServer($"Data Source={dataSource};" +
+                $" User Id={userId}; Initial Catalog={database}; Password={password}"));
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
