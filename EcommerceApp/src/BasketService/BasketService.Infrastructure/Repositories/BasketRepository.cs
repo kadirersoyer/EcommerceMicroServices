d0c49f8d1927712a88cbe3fd9c223e798481586b
kadirersoyer <kadirersoyer@gmail.com>
2022-11-05 16:55:21 +0300

    #BasketService#Redis External Ip Address Configured

--- EcommerceApp/src/BasketService/BasketService.Infrastructure/Repositories/BasketRepository.cs
+++ EcommerceApp/src/BasketService/BasketService.Infrastructure/Repositories/BasketRepository.cs
@@ -1,27 +1,27 @@
 ﻿using BasketService.Infrastructure.Entities;
 using BasketService.Infrastructure.Extensions;
-using Microsoft.Extensions.Caching.Distributed;
+using StackExchange.Redis;
 
 namespace BasketService.Infrastructure.Repositories
 {
     public class BasketRepository : IBasketRepository
     {
-        private readonly IDistributedCache _redisCache;
+        private readonly IDatabase _redisCache;
 
-        public BasketRepository(IDistributedCache redisCache)
+        public BasketRepository(IDatabase redisCache)
         {
             _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
         }
         public async Task DeleteBasket(string userName)
         {
-            var basket = await _redisCache.GetStringAsync(userName);
+            var basket = (await _redisCache.StringGetAsync(userName)).ToString();
             if (!string.IsNullOrEmpty(basket))
-                _redisCache.Remove(userName);
+                await _redisCache.StringGetDeleteAsync(userName);
         }
 
         public async Task<ShoppingCart> GetBasket(string userName)
         {
-            var basket = await _redisCache.GetStringAsync(userName);
+            var basket = (await _redisCache.StringGetAsync(userName)).ToString(); 
             if (string.IsNullOrEmpty(basket))
                 return null;
             return await basket.ToConvertToObj<ShoppingCart>();
@@ -30,8 +30,8 @@ namespace BasketService.Infrastructure.Repositories
         public async Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart)
         {
             var serializedData = await shoppingCart.ToConvertToString();
-            await _redisCache.SetStringAsync(shoppingCart.UserName, serializedData);
-            var basket = await _redisCache.GetStringAsync(shoppingCart.UserName);
+            await _redisCache.StringSetAsync(shoppingCart.UserName, serializedData);
+            var basket = (await _redisCache.StringGetAsync(shoppingCart.UserName)).ToString();
             if (string.IsNullOrEmpty(basket))
                 return new ShoppingCart(shoppingCart.UserName ?? string.Empty);
 
--- EcommerceApp/src/BasketService/BasketService.Shared/ServiceInjection.cs
+++ EcommerceApp/src/BasketService/BasketService.Shared/ServiceInjection.cs
@@ -2,18 +2,18 @@
 using BasketService.Infrastructure.Repositories;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
 namespace BasketService.Shared
 {
     public static class ServiceInjection
     {
         public static void Inject(IServiceCollection services, IConfiguration configuration)
         {
-            var redisConnectionString = configuration["CacheSettings:ConnectionString"];
-
-            services.AddStackExchangeRedisCache(opt =>
-            {
-                opt.Configuration = redisConnectionString;
-            });
+            var redisConnectionString = configuration["Redis_ExternalIpAddress"];
+            var redis = ConnectionMultiplexer.Connect(redisConnectionString);
+            
+            services.AddScoped(s => redis.GetDatabase());
 
             services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<IBasketService, BasketService.Business.Services.BasketService>();
